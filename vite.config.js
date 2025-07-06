4aa489caa5c5fe083633c9abca6555133de56541
Benchmarko <benchmarko@users.noreply.github.com>
2025-07-06 15:43:58 +0200

    set base path

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/GPXmap/', // <-- Set this to your repo name, with leading and trailing slash
+  // ...other config...
+});
