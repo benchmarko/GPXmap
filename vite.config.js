af8695b5c2e06b9e2e836d4057520c1aa282327e
Benchmarko <benchmarko@users.noreply.github.com>
2025-08-07 21:51:26 +0200

    vite: copy static examples gc*.js and update version in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
-  base: '/GPXmap/', // <-- Set this to your repo name, with leading and trailing slash
-  // ...other config...
+  base: '/GPXmap/',
+
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          version: pkg.version
+        }
+      }
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'examples/gc*.js',
+          dest: 'examples'
+        }
+      ]
+    })
+  ]
 });
