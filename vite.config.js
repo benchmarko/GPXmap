64ab2f5b0da5234aca82cccbfb4df7f444fc9abb
Benchmarko <benchmarko@users.noreply.github.com>
2025-09-14 09:03:52 +0200

    using vite-plugin-singlefile to allow using the app without a server

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from "vite-plugin-singlefile"
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
-  base: '/GPXmap/',
+  base: './',
+  
+  build: {
+    target: 'esnext',
+    // modulePreload: false,
+    // rollupOptions: {
+    //   output: {
+    //     format: "umd",
+    //     inlineDynamicImports: true,
+    //     entryFileNames: 'assets/[name].js',
+    //     assetFileNames: 'assets/[name].[ext]'
+    //   }
+    // }
+  },
 
   plugins: [
     createHtmlPlugin({
+      minify: false,
       inject: {
         data: {
           version: pkg.version
         }
       }
     }),
+    viteSingleFile(),
     viteStaticCopy({
       targets: [
         {
